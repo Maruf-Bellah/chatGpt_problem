a5474c4015b6100bf987f1dfa4c39be53de24f4f
marufbellah <mdmaruf5511507@gmail.com>
2023-09-20 17:36:28 +0600

    rumon vais problem solved

---
+++ Given_Problem/RumonVai/oneDay.js
@@ -0,0 +1,22 @@
+
+var a = 5;
+var b = 3;
+var c = 1;
+
+if(a > b && a > c){
+    console.log("A is greter then All = " + a);
+}else if(b > a && b > c){
+    console.log('B is greter then All = ' + b);
+}else if(c > b && c > a){
+    console.log('C is greter then All = ' + c);
+}
+
+
+if(a < b && a < c){
+    console.log("A is less then All = " + a);
+}else if(b < a && b < c){
+    console.log('B is less then All = ' + b);
+}else if(c < b && c < a){
+    console.log('C is less then All = ' + c);
+}
+
